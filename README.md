f6867f85a02752cce1239825d638e7f8f4e52505
Lesia <voloshka2912@icloud.com>
2024-05-30 20:27:51 +0300

    add solution

--- README.md
+++ README.md
@@ -1,9 +1,81 @@
-# React TypeScript Starter Pack
+# React To-Do App
 
-To use this template click `Use this template`
+## Overview
 
-### Available Scripts
+The React To-Do App is a simple yet powerful task management application built using React. It allows users to create, edit, delete, and mark tasks as completed.
 
-`Deploy` - available to deploy your application to gh-pages
+## Demo
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+Check out the [DEMO]() to see the project in action!
+
+## Technologies Used
+
+- **React:** The core framework used for building the user interface and managing state.
+- **TypeScript:** TypeScript is used to add static typing to JavaScript, providing better code quality and developer experience.
+- **SCSS:** SCSS is used for styling to enhance maintainability and modularity.
+- **GitHub Actions:** GitHub Actions are used for continuous integration and deployment, automating the build and deployment process.
+
+## Features
+
+### Task Management
+
+- **Create Tasks:** Easily create new tasks by typing in the task description and pressing enter or clicking the add button.
+- **Edit Tasks:** Update task details by clicking on the task name or description. This opens an editable field where you can make changes.
+- **Delete Tasks:** Remove tasks individually by clicking on the delete icon next to each task. There's also an option to delete all completed tasks at once.
+- **Mark as Completed:** Mark tasks as completed by checking the checkbox next to each task. Completed tasks are visually distinguished from active ones.
+
+### Data Management
+
+- **Local Storage:** Task data is stored locally using browser storage, ensuring that your tasks are saved even if you close the browser or refresh the page.
+
+## Getting Started
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+Node.js (v14.21.3)
+
+### Installation
+1. Clone the repository:
+```bash
+git clone https://github.com/vlesia/react-to-do-app.git
+```
+2. Navigate to the project directory:
+```bash
+cd nothing-landing
+```
+3. Install dependencies:
+```bash
+npm install
+```
+4. To run the project in your browser:
+```bash
+npm start
+```
+
+## Folder Structure
+
+The project follows a standard folder structure for better organization:
+
+```graphql
+react-to-do-app/
+├── public/               # Public assets
+├── src/                  # Source files
+│   ├── components/       # React components
+│   └── utils/            # Utility functions
+|   └── ...
+|
+│── .gitignore          # Git ignore file
+│── README.md           # Project documentation
+└── package.json        # Project metadata and dependencies
+```
+
+## Contributing
+
+We welcome contributions to the React To-Do App! If you have suggestions for new features, improvements, or bug fixes, please, fork the repository and create a pull request with your changes.
+
+
+## License
+
+This project is licensed under the MIT License.
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,209 @@
-import React from 'react';
-import './App.scss';
+import React, { useEffect, useRef, useState } from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import {
+  USER_ID,
+  creatTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from './api/todos';
+import { Todo } from './types/Todo';
+import { SortField } from './types/SortField';
+import { filterTodos } from './utils/helpers.ts/filterTodos';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
+import { TodoList } from './components/Todolist';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [filter, setFilter] = useState(SortField.All);
+  const [title, setTitle] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [processingIds, setProcessingIds] = React.useState<number[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const showErrorMessage = (errorMessage: string) => {
+    setError(errorMessage);
+    setTimeout(() => {
+      setError('');
+    }, 3000);
+  };
+
+  const filteredTodos = filterTodos(todos, filter);
+  const handleFilter = (filterValue: SortField) => {
+    setFilter(filterValue);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const normalizedTitle = title.trim();
+
+    if (!normalizedTitle) {
+      showErrorMessage('Title should not be empty');
+
+      return;
+    }
+
+    setIsDisabled(true);
+    setIsLoading(true);
+
+    try {
+      const newTodo = {
+        userId: USER_ID,
+        title: normalizedTitle,
+        completed: false,
+      };
+
+      setTempTodo({
+        id: 0,
+        ...newTodo,
+      });
+      const newCreatedTodo = await creatTodo(newTodo);
+
+      setTodos(prevTodos => [...prevTodos, newCreatedTodo]);
+      setTitle('');
+      setIsLoading(false);
+    } catch {
+      showErrorMessage('Unable to add a todo');
+    } finally {
+      setIsDisabled(false);
+      setTempTodo(null);
+      setIsLoading(false);
+    }
+  };
+
+  const updateTodoInList = (prevTodos: Todo[], updatedTodo: Todo): Todo[] => {
+    return prevTodos.map(todo =>
+      todo.id === updatedTodo.id ? updatedTodo : todo,
+    );
+  };
+
+  const toggleTodo = async (todoToUpdate: Todo) => {
+    setProcessingIds(prevIds => [...prevIds, todoToUpdate.id]);
+
+    try {
+      const updatedTodo = await updateTodo({
+        ...todoToUpdate,
+        completed: !todoToUpdate.completed,
+      });
+
+      setTodos(prevTodos => updateTodoInList(prevTodos, updatedTodo));
+    } catch {
+      showErrorMessage('Unable to update a todo');
+    } finally {
+      setProcessingIds(prevIds => prevIds.filter(id => id !== todoToUpdate.id));
+    }
+  };
+
+  const toggleAll = () => {
+    const activeTodos = todos.filter(todo => !todo.completed);
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    if (activeTodos.length !== 0) {
+      activeTodos.forEach(todo => toggleTodo(todo));
+    } else {
+      completedTodos.forEach(todo => toggleTodo(todo));
+    }
+  };
+
+  const renameTodo = async (todoToRename: Todo, newTitle: string) => {
+    setProcessingIds(prevIds => [...prevIds, todoToRename.id]);
+    try {
+      const updatedTodo = await updateTodo({
+        ...todoToRename,
+        title: newTitle,
+      });
+
+      setTodos(prevTodos => updateTodoInList(prevTodos, updatedTodo));
+    } catch (newError) {
+      showErrorMessage('Unable to update a todo');
+      throw newError;
+    } finally {
+      setProcessingIds(prevIds => prevIds.filter(id => id !== todoToRename.id));
+    }
+  };
+
+  const handleDeleteTodo = async (todoId: number) => {
+    setProcessingIds(prevIds => [...prevIds, todoId]);
+
+    try {
+      await deleteTodo(todoId);
+      setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+    } catch {
+      showErrorMessage('Unable to delete a todo');
+    } finally {
+      setProcessingIds(prevIds => prevIds.filter(id => id !== todoId));
+    }
+  };
+
+  const handleClearCompleted = () => {
+    todos.forEach(todo => {
+      if (todo.completed) {
+        handleDeleteTodo(todo.id);
+      }
+    });
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        showErrorMessage('Unable to load todos');
+      });
+  }, []);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [todos, isLoading]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+          title={title}
+          isDisabled={isDisabled}
+          inputRef={inputRef}
+          toggleAll={toggleAll}
+          todos={todos}
+        />
+
+        <TodoList
+          todos={filteredTodos}
+          handleDeleteTodo={handleDeleteTodo}
+          tempTodo={tempTodo}
+          toggleTodo={toggleTodo}
+          renameTodo={renameTodo}
+          processingIds={processingIds}
+        />
+
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            filter={filter}
+            handleFilter={handleFilter}
+            handleClearCompleted={handleClearCompleted}
+          />
+        )}
+      </div>
+      <ErrorNotification error={error} setError={setError} />
     </div>
   );
 };
